377342283b243b3f39aba9f00125497b3a0d04f2
Mark Kashirskiy <mekashirskiy@avito.ru>
2026-01-28 19:50:29 +0300

    Add USE_MLFLOW toggle

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from ml.model import train_model, save_model, load_model
+from ml.model import (
+    train_model,
+    save_model,
+    load_model,
+    register_model,
+    promote_to_production,
+    load_from_mlflow,
+)
 from routers.users import router as user_router
 
 logging.basicConfig(
@@ -14,6 +21,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 MODEL_PATH = "model.pkl"
+USE_MLFLOW = os.getenv("USE_MLFLOW", "false").lower() == "true"
 
 
 @asynccontextmanager
@@ -22,8 +30,16 @@ async def lifespan(app: FastAPI):
         logger.info("Training new model")
         model = train_model()
         save_model(model, MODEL_PATH)
-    app.state.model = load_model(MODEL_PATH)
-    logger.info("Model loaded")
+        register_model(model)
+        promote_to_production()
+        logger.info("Model registered in MLflow")
+
+    if USE_MLFLOW:
+        app.state.model = load_from_mlflow()
+        logger.info("Model loaded from MLflow")
+    else:
+        app.state.model = load_model(MODEL_PATH)
+        logger.info("Model loaded from pickle")
     yield
     app.state.model = None
 
